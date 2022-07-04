8a0a83e64d55b6a3fcd6858a315035d0b10ba604
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-04 04:51:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
