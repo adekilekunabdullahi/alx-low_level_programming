40674bb4a93412fc997b7f78ad6110f6af8f337b
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-05 15:18:21 -0700

    hood

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #include "main.h"
 /**
  *main - Entry point
@@ -8,7 +8,7 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');
+/*	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -17,7 +17,7 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-
+*/
 	return (0);
 }
 
