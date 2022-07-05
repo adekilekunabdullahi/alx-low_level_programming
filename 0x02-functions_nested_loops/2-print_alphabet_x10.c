0a3c95405887fc7d4db433d018d4eac60326ed8d
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-05 16:57:09 -0700

    i

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		int j = 'a';
+
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		x++;
+	}
+	return (0);
+}
