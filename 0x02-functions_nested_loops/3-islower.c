be746282e7161c3c08982d7ac08895e84f6796c2
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-06 05:02:24 -0700

    sup

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,45 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - clone
  * test_islower - you'll
- *
+ * @c: unique
  * Return: Always 0.
  *
  */
 
-int main(void)
+int test_islower(int c)
 {
-	int r
-	int test_islower(int c)
+	if (c >= 97 && c <= 122)
 	{
-		if (c >= 97 && c <= 122)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	};
-	r = test_lower(108);
-	_putchar(r +'0');
-	r = test_lower('c');
-	_putchar(r +'0');
-	r = test_lower('I');
-	_putchar(r +'0');
-	r = test_lower('s');
-	_putchar(r +'0');
-	r = test_lower(70);
-	_putchar(r +'0');
-	r = test_lower(20);
-	_putchar(r +'0');
-	r = test_lower('U');
-	_putchar(r +'0');
-	r = test_lower('n');
-	_putchar(r +'0');
-	r = test_lower('o');
-	_putchar(r +'0');
-	_putchar('\n');
-	return (0);
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
