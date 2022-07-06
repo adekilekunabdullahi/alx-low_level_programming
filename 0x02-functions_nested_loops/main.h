81d95a77e61f36c3d1858f4dcac962a963dc00cc
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-06 10:53:26 -0700

    sup

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,6 @@
 *Return: on success 1.
 *on error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
