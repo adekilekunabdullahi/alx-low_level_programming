e103d4cb90b2615bf653f73d0ef56fc71aff7247
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-06 05:08:06 -0700

    sup

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * test_islower - you'll
+ * _islower - you'll
  * @c: unique
  * Return: Always 0.
  *
  */
 
-int test_islower(int c)
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
