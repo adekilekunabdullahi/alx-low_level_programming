c2adee8a23aef76d4c2710fb07cb78f7a9a14295
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-07 04:21:07 -0700

    twas

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - function
+ *@c: parameter
+ * Return: always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
