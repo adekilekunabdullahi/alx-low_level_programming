4c5cb39c646f85c0708ac0e432f7b60a9176cee3
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-12 16:46:37 -0700

    tween

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,6 +7,5 @@
  */
 void _puts(char *str)
 {
-	puts(str);
-	_putchar('\n');
+	put(str);
 }	
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - a function
+ * @s: a manner
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
