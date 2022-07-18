6a9518ce7ea30b924069f28fb9d7fb3d90c237a9
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-18 06:54:13 -0700

    sup

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_memcpy - a recipient
+ * @dest: global
+ * @src: a party
+ * @n: desktop
+ * Return: master
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+		src[i] = dest[i];
+	return (dest);
+}
