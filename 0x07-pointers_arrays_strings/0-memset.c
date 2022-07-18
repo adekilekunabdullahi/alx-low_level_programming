d3c36f217eaee96c60ea980a80b369313aea0f32
Adekilekun Abdullahi <adekilekunabdullahi@gmail.com>
2022-07-18 04:39:01 -0700

    twas

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_memset - a recipient
+ * @s: global
+ * @b: a party
+ * @n: desktop
+ * Return: master
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+}
