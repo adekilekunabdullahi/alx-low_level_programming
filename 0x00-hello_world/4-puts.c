23325f7a8f17af7ae12e4da1555a53a42e3fd0f6
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-08 12:05:04 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,6 @@
  */
 int main(void)
 {
-
-	
-	puts("\"programming is like building a multilingual puzzle\n");
-        
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
