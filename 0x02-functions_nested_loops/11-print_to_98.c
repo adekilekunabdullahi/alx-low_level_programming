6654b60c833a01526674bf8ca8d31d7adec9e3ad
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-13 22:10:45 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d", i);
-			if (i > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d%s ", i > 98 ? ',':'')
 		}
 	}
 	else if (n < 98)
