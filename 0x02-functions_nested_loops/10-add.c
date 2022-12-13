37501fccf0950f772a70b48f551cc266147a6934
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-13 11:59:53 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return sum;
+}
+int main(void)
+{
+	    int n;
+
+	        n = add(89, 9);
+		    printf("%d\n", n);
+		        return (0);
+}
