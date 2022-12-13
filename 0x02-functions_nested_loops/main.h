629050fd502b976f31224980739e0b6820b7681c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-13 08:20:10 -0800

    update file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,12 @@
 int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+void print_alphabet_x10(void);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
