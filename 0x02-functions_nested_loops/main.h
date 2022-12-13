08d66b58d9e6d4ace9b8e4ca449ee223847e8f9b
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-13 17:55:06 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 void print_alphabet_x10(void);
-int _abs(int);
+int abss(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
