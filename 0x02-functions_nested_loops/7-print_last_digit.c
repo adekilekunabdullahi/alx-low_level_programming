9ed654088e174baf3e21f9f44db39370ea7dae33
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-13 23:44:34 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,27 @@ int print_last_digit(int a)
 {
 	int b;
 
-	b = a % 10;
-	return (b);
+	if (a > 0)
+	{
+		b = a % 10;
+		_putchar(b + '0');
+		return (b);
+	}
+	else
+	{
+		b = -(a % 10);
+		_putchar(b + '0');
+		return (b);
+	}
+}
+int main(void)
+{
+	int r;
+
+	    print_last_digit(98);
+	        print_last_digit(0);
+		    r = print_last_digit(-1024);
+		        _putchar('0' + r);
+			    _putchar('\n');
+			        return (0);
 }
