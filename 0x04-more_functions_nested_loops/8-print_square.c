8df02ab8aea9198e73fff98361d28c75b4c8a8c4
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-15 11:54:02 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
  *print_square - a function that draws a straight line in the terminal.
- * @n: a parameter
+ *@n: a parameter
  */
 void print_square(int n)
 {
 	int i;
+
+	int b;
 
 	if (n < 0 || n == 0)
 		_putchar('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
-			for (i = 0; i < n; i++)
+		{
+			for (b = 0; b < n; b++)
 				_putchar('#');
 			_putchar('\n');
-		_putchar('\n');
+		}
 	}
 }
