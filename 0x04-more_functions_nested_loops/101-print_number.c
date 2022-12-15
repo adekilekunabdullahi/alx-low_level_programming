dff2d6e04d74d3ff1967b8ed7140988f7bb2e5b6
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-16 00:58:13 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,10 @@ void print_number(int a)
 	if (a < 0)
        	{
 		_putchar('-');
-		a = -a;
 	}
 
 	if (a == 0)
-		_putchar('0');
+		_putchar('\0');
 	if (a / 10)
 		print_number(a / 10);
 	_putchar(a % 10 + '0');
