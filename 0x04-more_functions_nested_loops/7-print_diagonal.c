c247600125052dadcf2fa601cb67791842da9a4c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-15 10:25:31 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_diagonal - a function that draws a straight line in the terminal.
+ * @n: a parameter
  */
 void print_diagonal(int n)
 {
@@ -9,8 +10,10 @@ void print_diagonal(int n)
 	if (n < 0 || n == 0)
 		_putchar('\n');
 	else
+	{
 		for (i = 0; i < n; i++)
 			_putchar('\');
 		_putchar('\n');
+	}
 }
 
