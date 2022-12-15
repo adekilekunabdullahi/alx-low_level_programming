09da5a61311bf3bc7ed1e312ce812dc30937fe0c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-15 14:49:08 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * more numbers - a function
+ * more_numbers - a function
  *
  */
 void more_numbers(void)
 {
-	char a;
+	int a;
 
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (a = '0'; a <= '14'; a++)
+		for (a = 0; a <= 14; a++)
 		{
-			_putchar(a);
+			printf("%d", a);
 		}
 		_putchar('\n');
 	}
-}
-int main(void)
-{
-	more_numbers();
-	return (0);
 }
