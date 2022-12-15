2d65ce66baf9166aeb710566bca0fe7c484a09a7
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-15 15:54:09 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - a function that prints number without printf
+ * @a: a parameter
+ */
+void print_number(int a)
+{
+	if (a < 0)
+       	{
+		_putchar('-');
+		a = -a;
+	}
+
+	if (a == 0)
+		_putchar('0');
+	if (a / 10)
+		print_number(a / 10);
+	_putchar(a % 10 + '0');
+}
