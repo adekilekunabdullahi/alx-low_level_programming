67aef55fccc0e674ba1f41bac1ccec4bf03c3bc4
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-19 02:30:20 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,25 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
+	int a, len, c,  b;
+
+	len = strlen(s);
+	c = len - 1;
+	for (a = 0; a < len - 1; a++)
+	{
+		b = s[a];
+		s[a] = s[len -1];
+		s[len - 1] = b;
+		c--;
+		_putchar(b);
+	}
+
+}
+int main(void)
+{
+	char *str;
+
+	    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	        print_rev(str);
+		    return (0);
 }
