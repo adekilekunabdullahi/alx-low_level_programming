a71227c19f3fdcf288ea725764b7438cfa78de6d
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-19 12:52:19 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -24,12 +24,4 @@ void print_rev(char *s)
 	 	end_ptr--;
  	}
 }
-int main(void)
-{
-	    char *str;
-
-	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-		    print_rev(str);
-		        return (0);
-}
 
