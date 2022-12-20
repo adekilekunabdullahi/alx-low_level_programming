af64d5b9947c535c73c7d6d59ae8cf8cbd7158a5
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-20 01:26:19 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,28 @@
  */
 void puts_half(char *str)
 {
-	size_t len, i, j, mid;
+	size_t len, i, j, mid, a;
+
+	char right[250];
 
 	len = strlen(str);
+	mid = len / 2;
 	if (len % 2 == 0)
 	{
-		mid = len / 2;
-		for (i = mid; i <= len; i++)
-			_putchar(str[i]);
+		for (i = mid; a = 0;  i <= len; i++, a++)
+		{
+			right[a] = str[i];
+			_putchar(right[a]);
+		}
 	}
 	else
 	{
 		j = (len - 1) / 2;
-		for (i = j; i <= len; i++)
-			_putchar(str[i]);
+		for (i = j; a = 0; i <= len; i++, a++)
+		{
+			right[a] = str[i];
+			_putchar(right[a]);;
+		}
 	}
 	/*_putchar('\n');*/
 }
