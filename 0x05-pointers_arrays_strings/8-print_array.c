d343899f870d7811796315e2ce926f43c2db71b6
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-20 09:26:07 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,10 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", a[i], i < n - 1 ? ", ": "");
+		printf("%d%s", a[i], i < n - 1 ? ", " : "");
 	}
 	printf("\n");
 }
