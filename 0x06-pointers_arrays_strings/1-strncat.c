63d9bd7825dcf4a062f747ed9ba3b91851389441
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2022-12-21 18:23:00 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -24,21 +24,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i + a] = '\0';
 	return (dest);
 }
-int main(void)
-{
-	    char s1[98] = "Hello ";
-	        char s2[] = "World!\n";
-		    char *ptr;
-
-		        printf("%s\n", s1);
-			    printf("%s", s2);
-			        ptr = _strncat(s1, s2, 1);
-				    printf("%s\n", s1);
-				        printf("%s", s2);
-					    printf("%s\n", ptr);
-					        ptr = _strncat(s1, s2, 1024);
-						    printf("%s", s1);
-						        printf("%s", s2);
-							    printf("%s", ptr);
-							        return (0);
-}
