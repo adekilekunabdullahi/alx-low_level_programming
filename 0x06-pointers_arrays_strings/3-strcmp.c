9bab6ac70cecdc477beb9ad3c04859bc51eae535
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-21 11:52:50 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: first parameter to compare
+ * @s2: second parameter to compare
+ *Return: int value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	unsigned char a, b;
+
+	do {
+		a = (unsigned char) *s1++;
+		b = (unsigned char) *s2++;
+		if (a == '\0')
+			return (a - b);
+	}
+	while (a == b)
+	{
+	}
+	return (a - b);
+}
