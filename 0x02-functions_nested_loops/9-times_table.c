47d85ffdd094f8dda51ab4cfccc0f9cc78251345
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2022-12-25 13:51:38 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+void times_table(void)
+{
+	int a;
+
+	int b;
+
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			printf("%d%s", c , b < 9 ? ", " : "");
+		}
+		puts("");
+	}
+	puts("");;
+}
+int main(void)
+{
+	times_table();
+	return (0);
+}
