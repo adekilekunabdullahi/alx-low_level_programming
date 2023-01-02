8f129a14ecf24ae1c692edc952782c30463be7cb
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-02 06:02:59 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strchr - a function that locates a character in a string.
+ *@s: strings to search from.
+ *@c: character to search.
+ * Return: position of c.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+		s++;
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
+}
