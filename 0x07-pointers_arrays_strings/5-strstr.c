dae8e9603753e14bdc488fc9258a68921ac8d1d8
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-02 08:17:55 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *compare - a function that compares two strings
+ *@X: String 1
+ *@Y: String 2
+ *Return: searched strings.
+ */
+int compare(const char *X, const char *Y)
+{
+	while (*X && *Y)
+	{
+		if (*X != *Y)
+		{
+			return (0);
+		}
+		X++;
+		Y++;
+	}
+	return (*Y == '\0');
+}
+/**
+ *_strstr - a function that locates a substring
+ *compare - a function that compares two strings
+ *@X: String 1
+ *@Y: String 2
+ *Return: searched strings.
+ */
+char *_strstr(char *X, char *Y)
+{
+	while (*X != '\0')
+	{
+		if ((*X == *Y) && compare(X, Y))
+		{
+			return (X);
+		}
+		X++;
+	}
+	return (NULL);
+}
