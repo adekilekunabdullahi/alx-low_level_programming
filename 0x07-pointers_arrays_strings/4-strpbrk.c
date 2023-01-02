35b89c19ba3acdd95cf5e0431d36f05b2da64a8a
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-02 07:40:27 -0800

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,36 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *_strpbrk - a function that searches a string for any of a set of bytes.
+ *@s: strings to searched from.
+ *@accept: sets of bytes.
+ *Return: sets of byte found.
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j, pos, flag = 0;
-    for (i = 0; s[i] != '\0';i++);
-    pos = i;
-    for (i = 0;accept[i] != '\0';i++)
-    {
-        for (j = 0;s[j] != '\0';j++)
-        {
-            if (accept[i] == s[j])
-            {
-                if (j <= pos)
-                {
-                    pos = j;    
-                    flag = 1;    
-                }
-            }
-         }        
-    }
-    if (flag == 1)
-    {
-        return &s[pos];
-    }
-    else
-    {
-        return NULL;
-    }
+	int i, j, pos, flag = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	pos = i;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				if (j <= pos)
+				{
+					pos = j;
+					flag = 1;
+				}
+			}
+		}
+	}
+	if (flag == 1)
+	{
+		return (&s[pos]);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
