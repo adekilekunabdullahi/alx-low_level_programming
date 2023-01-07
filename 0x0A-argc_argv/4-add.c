329eac3f7986c334629d8b5b3a28d2f1818a82e3
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-07 07:02:32 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+/**
+ * main - function that prints its name
+ * @argc: - number of parameters.
+ * @argv: arrays of parameters.
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
 	int i, adds = 0;
 
-	for (i = 0; i < argc; i++)
-		adds = adds + atoi(argv[i]);
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(*argv[i]))
+			adds = adds + atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
 	printf("%d\n", adds);
 	return (0);
 }
