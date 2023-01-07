13f8f963c88258d29d1ee7fa5ba7d5c4f455520b
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-07 13:43:32 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - function that prints its name
+ * @argc: - number of parameters.
+ * @argv: arrays of parameters.
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
-	if (*argv)
-		printf("%i",argc - 1);
+	if (*argv != 0)
+		printf("%d\n", argc - 1);
 	return (0);
 }
