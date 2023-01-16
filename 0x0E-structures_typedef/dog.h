df1f93421a3139c19800b6e453ab3d5dc76519ab
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-16 13:32:51 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-#include "main.h"
 #ifndef dogs
 #define dogs
 /**
@@ -13,4 +12,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
