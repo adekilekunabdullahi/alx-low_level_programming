29d3aa2f086d97849bf55f6ff25f33f23e33e44e
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-16 13:20:44 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *free_dog - a function that free dog
+ *@d: a parameter
+ */
+void free_dog(dog_t *d);
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
