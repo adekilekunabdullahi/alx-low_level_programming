383a936dc2fca49c77b9c1c4089d8e389f2c30cf
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-18 11:48:51 -0800

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef AB
 #define AB
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
