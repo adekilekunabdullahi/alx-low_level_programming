ceabf3f825abc01f0e62149397a6648b889e7318
Adekilekun Abdullahi <106445363+adekilekunabdullahi@users.noreply.github.com>
2023-01-18 19:31:37 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,11 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+void print_elem(int elem);
+void print_elem_hex(int elem);
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 #endif
