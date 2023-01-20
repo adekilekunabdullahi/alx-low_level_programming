0556cdc7220b3301bf93266485ed39d67e556bdf
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-20 14:41:12 -0800

    updates

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_c - Prints a character of char type
+ *@separator: The separator of the character
+ *@ch: A list of variadic arguments
+ */
+void print_c(char *separator, va_list ch)
+{
+	printf("%s%c", separator, va_arg(ch, int));
+}
+/**
+ *print_s - Prints a strings
+ *@separator: The separator of the character
+ *@s: A list of variadic arguments
+ */
+void print_s(char *separator, va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str)
+		printf("%s%s", separator, str);
+	else
+		printf("%s%s", separator, "(nil)");
+}
+/**
+ *print_i - Prints an integer
+ *@separator: The separator of the character
+ *@i: A list of variadic arguments
+ */
+void print_i(char *separator, va_list i)
+{
+	printf("%s%d", separator, va_arg(i, int));
+}
+/**
+ *print_f - Prints a float
+ *@separator: The separator of the character
+ *@f: A list of variadic arguments
+ */
+void print_f(char *separator, va_list f)
+{
+	printf("%s%f", separator, va_arg(f, double));
+}
+/**
+  * print_all - Prints anything
+  * @format: The conversion specifier to prints
+  *
+  * Return: Nothing
+  */
+void print_all(const char * const format, ...)
+{
+
+	int i, j;
+	char *separator = "";
+	va_list p;
+	ab e[] = {
+	{"c", print_c},
+	{"i", print_i},
+	{"f", print_f},
+	{"s", print_s}
+	};
+
+	va_start(p, format);
+	i = 0;
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *e[j].c)
+			{
+				e[j].d(separator, p);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(p);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,18 @@
 #ifndef A
 #define A
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct abc - a structure
+ * @c: a string
+ * @d: pointer to function
+ */
+typedef struct abc
+{
+	char *c;
+	void (*d)(char *separator, va_list p);
+} ab;
 #endif
