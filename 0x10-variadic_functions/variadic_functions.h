d077115fe3b3dfca74ac75b7adbc939fd13ee9d6
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-22 06:19:53 -0800

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,11 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_f(char *separator, va_list p);
+int _putchar(char c);
+void print_s(char *separator, va_list p);
+void print_i(char *separator, va_list p);
+void print_c(char *separator, va_list p);
 /**
  * struct abc - a structure
  * @c: a string
