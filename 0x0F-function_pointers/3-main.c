a4b5a9a6dbd01f9fe71f4fdf761951f606cbe496
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-23 02:31:13 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,14 +10,27 @@
 int main(int argc, char **argv)
 {
 	int r;
+	int (*q)(int, int);
 	char *a = argv[argc - 2];
 	int b = atoi(argv[argc - 1]);
 	int c = atoi(argv[argc - 3]);
 
-	if (argc > 4)
+	q = get_op_func(a);
+
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+	if ((*a == '/' || *a == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (!q)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 	r = get_op_func(a)(c, b);
 	printf("%d\n", r);
