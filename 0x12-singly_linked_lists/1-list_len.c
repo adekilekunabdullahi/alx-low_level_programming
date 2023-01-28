eeb636a5f862c4c1d880aa402d054b3f172ed28e
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-27 19:13:52 -0800

    refactor

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
  *list_len - a function that returns the number of elements in a linked list.
@@ -7,7 +8,7 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h)
 	{
