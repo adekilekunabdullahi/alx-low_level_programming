7c896055664d615d1029e001d83d611d9c627d1f
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-27 20:51:11 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,11 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 1;
 
-	while (h != NULL)
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -19,5 +21,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		i++;
 	}
+	printf("[%d] %s\n", h->len, h->str);
 	return (i);
 }
