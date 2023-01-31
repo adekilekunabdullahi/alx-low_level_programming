d20d38d6077af910808a229e653b5d8e9c289d03
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-01-31 12:14:12 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int i = 0;
+	int i = 1;
 
 	if (!h)
 		return (0);
-	while (h)
+	while (h->next)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
+	printf("%d\n", h->n);
 	return (i);
 }
