766f8b71c2ab89866e5a0af5c121cd027d951ea0
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-01 12:01:17 -0800

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	listint_t *node;
 
 	node = head;
 	if (!head)
 		return (NULL);
+
 	while (node)
 	{
 		if (i == index)
