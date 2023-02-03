9a5a9d899d88353b63f5e1c0b6660c68fb7d272c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-03 00:49:55 -0800

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * countSetBits - a function that count set bits
+ * @a: number
+ * Return: number of set bits
+ */
+int countSetBits(int a)
+{
+	int count = 0;
+
+	while (a > 0)
+	{
+		count++;
+		a &= (a - 1);
+	}
+	return (count);
+}
+/**
+ * flip_bits - function that returns the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: number 1
+ * @m: , 2nd number.
+ * Return: number of b its you would need to
+ * flip to get from one number to another
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countSetBits(n ^ m));
+}
