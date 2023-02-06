faa24fcbd6fa64e9d38c91bf1b06d4ae41f48abd
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-06 09:36:14 -0800

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,17 +8,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, a, d;
+	int fd, d;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND, 0);
 	if (fd == -1)
 		return (-1);
-	for (a = 0; text_content[a]; a++);
 	if (text_content)
 	{
-		d = write(fd, text_content, a);
+		d = write(fd, text_content, _strlen(text_content));
 		if (d == -1)
 			return (-1);
 	}
@@ -26,4 +25,19 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	close(fd);
 	return (1);
+}
+/**
+ * _strlen - function to calculate the length of a string
+ *@s: string.
+ *Return: length of the string.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+	return (a);
 }
