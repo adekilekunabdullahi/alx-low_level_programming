1f18be4a78d064704911909a8f2e88eb7090d997
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-06 14:30:45 -0800

    updates

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include <fcntl.h>
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - main program to copy strings from one file to Another
+ * @argc: ...
+ * @argv: ...
+ * Return: success or failure.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		exit(97);
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+	}
+	copy_string(argv[1], argv[2]);
+	return (0);
+}
+/**
+ * copy_string - ...
+ * @file_from: ...
+ * @file_to: ...
+ */
+void copy_string(const char *file_from, const char *file_to)
+{
+	char *ptr;
+	int cd, fd, fd1;
+
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1 || access(file_from, F_OK) != 0)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from file", file_from);
+	}
+	ptr = malloc(sizeof(char *) * 1024);
+	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((cd = read(fd, ptr, 1024)) > 0)
+	{
+		if (fd1 == -1 || write(fd1, ptr, cd) != cd)
+		{
+			exit(99);
+			dprintf(STDERR_FILENO, "%s %s\n", "Can't write to", file_to);
+		}
+	}
+
+	if (close(fd) == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "%s %d\n", "Can't close fd", fd);
+	}
+	if (close(fd1) == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "%s %d\n", "Can't close fd", fd1);
+	}
+	free(ptr);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
+void copy_string(const char *file_from, const char *file_to);
 #endif
