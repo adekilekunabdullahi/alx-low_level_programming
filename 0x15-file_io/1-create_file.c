f6256ef5c190e225b001241598f238a722e6c1fb
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-06 06:58:54 -0800

    updatea

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
-	if (file < 0)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
 	while (text_content[a])
 	{
