c6ce8199bf35f3c957bb8e2fe9c6e4bbaec070ef
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-06 11:00:46 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,17 @@ int _strlen(char *s);
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, b;
+	int file;
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
-	{
-
-		file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (file == -1)
-			return (-1);
-	}
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
-	b = write(file, text_content, _strlen(text_content));
-	if (b == -1)
-		return (-1);
+	if (text_content)
+	{
+		write(file, text_content, _strlen(text_content));
+	}
 	close(file);
 	return (1);
 }
