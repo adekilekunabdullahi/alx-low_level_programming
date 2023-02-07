7d03e80f984e3f213383d6db5bc7aa86113a169f
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-02-07 03:01:27 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,8 +13,8 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
+		write(2, "Usage: cp file_from file_to\n", 28);
 		exit(97);
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	}
 	copy_string(argv[1], argv[2]);
 	return (0);
@@ -32,16 +32,16 @@ void copy_string(const char *file_from, const char *file_to)
 	fd = open(file_from, O_RDONLY);
 	if (fd == -1 || access(file_from, F_OK) != 0)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 	fd1 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((cd = read(fd, ptr, 1024)) > 0)
 	{
 		if (fd1 == -1 || write(fd1, ptr, cd) != cd)
 		{
-			exit(99);
 			dprintf(STDERR_FILENO, "Can't write to %s\n", file_to);
+			exit(99);
 		}
 	}
 	if (cd == -1)
@@ -51,12 +51,12 @@ void copy_string(const char *file_from, const char *file_to)
 	}
 	if (close(fd) == -1)
 	{
-		exit(100);
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
+		exit(100);
 	}
 	if (close(fd1) == -1)
 	{
-		exit(100);
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd1);
+		exit(100);
 	}
 }
