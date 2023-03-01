767d77212751b1a9f90e76c30ccc8c24f7e97264
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-01 02:06:49 -0800

    reverting back!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char s[64];
+
+	strcpy(s, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "%s\n", s);
 	return (1);
 }
