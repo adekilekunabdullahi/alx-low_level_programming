427945544e538e71607950b35c137ef5c8236d5f
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-02 02:02:18 -0800

    8-24_hours.c project files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,22 +3,26 @@
 /**
  * jack_bauer - a function that prints every minute of the day of Jack Bauer
  * starting from 00:00 to
- *
- *
  */
 void jack_bauer(void)
 {
 	int a = 0;
+	int b = 0;
 
-	int b;
-
-	while (a < 24)
+	for (a = 0; a < 24; a++)
 	{
 		for (b = 0; b < 60; b++)
 		{
-			printf("%d:%d", a, b);
-			puts("");
+			if (a == 0)
+			{
+				putchar('0');
+			}
+			printf("%d:", a);
+			if (b < 10)
+			{
+				putchar('0');
+			}
+			printf("%d\n", b);
 		}
-		a++;
 	}
 }
