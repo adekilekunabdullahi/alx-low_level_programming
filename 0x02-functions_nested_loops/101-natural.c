e7073ed6bdfa581c1e350eb87b47cff633246c4f
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-02 05:47:57 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * sumOfMultiples - function that calculates the sum of all the multiples of 3
+ * or 5 below 1024 (excluded), followed by a new line.
+ */
+void sumOfMultiples(void)
+{
+	int a[2] = {3, 5};
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((1024 / i) == a[0] || (1024 / i) == a[1])
+			sum += i;
+	}
+	printf("%d\n", sum);
+}
+/**
+ * main - entry point
+ * Return: success
+ */
+int main(void)
+{
+	sumOfMultiples();
+	return (0);
+}
