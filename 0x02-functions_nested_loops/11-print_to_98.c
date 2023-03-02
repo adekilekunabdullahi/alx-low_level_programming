53307da95c375a2c1a6ddd605373ed5887b195ba
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-02 03:32:05 -0800

    refactored

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d%s", i, i > 98 ? ", " : "");
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
 		}
 		puts("");
 	}
@@ -21,7 +23,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d%s", i, i < 98 ? ", " : "");
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
 		}
 		puts("");
 	}
