77215f36399b8560c987c341f6d56cf0b36f4a05
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-16 07:49:32 -0700

    updates

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	printf("%d\n", h->n);
-	i++;
 	return (i);
 }
